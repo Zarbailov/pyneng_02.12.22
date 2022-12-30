01c266d5935d4b42309e663f5b603644ff3a7130
fay-day <g.zarbailov@gmail.com>
2022-12-30 13:19:23 -0600

    done task_17_3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,27 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+
+import re
+
+
+def parse_sh_cdp_neighbors(output):
+    regex = re.compile(
+        r"(?P<device>\S+) +"
+        r"(?P<l_intf>\S+ [0-9]/[0-9]) +\d+.+?"
+        r"(?P<port_id>\S+ [0-9]/[0-9])"
+    )
+    result = {}
+    for line in output.split("\n"):
+        if "show cdp neighbors" in line:
+            hostname = line.split(">")[0]
+            result[hostname] = {}
+        match = regex.search(line)
+        if match:
+            device, l_intf, port_id = match.groups()
+            result[hostname][l_intf] = {device : port_id}
+    return result
+
+if __name__ == "__main__":
+    with open(("E:/development/pyneng/17/sh_cdp_n_sw1.txt")) as f:
+        pprint(parse_sh_cdp_neighbors(f.read()))
