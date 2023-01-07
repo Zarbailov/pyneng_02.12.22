c9813bc7c1486993ed35188b4b79dc56d416ca87
fay-day <g.zarbailov@gmail.com>
2023-01-07 01:36:14 -0600

    done task_19_1

--- exercises/19_concurrent_connections/task_19_1.py
+++ exercises/19_concurrent_connections/task_19_1.py
@@ -23,3 +23,42 @@
 а затем запустить эту функцию в разных потоках для разных
 IP-адресов с помощью concurrent.futures (это надо сделать в функции ping_ip_addresses).
 """
+
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+
+def ping(ip):
+    reply = subprocess.run(['ping', '-c', '3', '-n', ip],
+                            stdout=subprocess.DEVNULL,
+                            stderr=subprocess.DEVNULL,
+                            encoding='utf-8'
+    )
+    return {ip : reply.returncode}
+
+
+
+
+
+
+
+def ping_ip_addresses(ip_list, limit=3):
+    reachable = []
+    unreachable = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result = executor.map(ping, ip_list)
+        for ip_dict in result:
+            for keys, values in ip_dict.items():
+                if values == 0:
+                    reachable.append(keys)
+                else:
+                    unreachable.append(keys)
+    return reachable, unreachable
+
+
+ip_addr = ["10.20.30.40", "8.8.8.8",
+           "11.22.33.44", "192.168.100.1",
+           "192.168.100.2", "192.168.100.3"]
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(ip_addr))
